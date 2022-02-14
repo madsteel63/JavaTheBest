da11ef0030cf53e9d4e26ae5e88541ce14fb3e27
madsteel63 <silverlightoffallingstar@gmail.com>
2022-02-14 16:35:23 +0400

    Последняя версия

--- src/Stage04.java
+++ src/Stage04.java
@@ -1,6 +1,6 @@
 public class Stage04 {
     public static void main(String[] args) {
-        System.out.println("Результаты логических операций с двумя boolean переменными:\n=====================");
+        System.out.println("Р РµР·СѓР»СЊС‚Р°С‚С‹ Р»РѕРіРёС‡РµСЃРєРёС… РѕРїРµСЂР°С†РёР№ СЃ РґРІСѓРјСЏ boolean РїРµСЂРµРјРµРЅРЅС‹РјРё:\\n=====================");
         for (int i = 1; i <= 30; i++){
             System.out.print(i + "-\t" + i%10 + "\t  ");
             if (i % 10 == 0){System.out.println();}
