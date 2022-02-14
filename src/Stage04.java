a43609e4efc331f7e9a429016f2ac8b93f5ca562
madsteel63 <silverlightoffallingstar@gmail.com>
2022-02-14 16:21:17 +0400

    На русскую раскладку

---
+++ src/Stage04.java
@@ -0,0 +1,10 @@
+public class Stage04 {
+    public static void main(String[] args) {
+        System.out.println("Результаты логических операций с двумя boolean переменными:\n=====================");
+        for (int i = 1; i <= 30; i++){
+            System.out.print(i + "-\t" + i%10 + "\t  ");
+            if (i % 10 == 0){System.out.println();}
+        }
+
+    }
+}
