615823f4fbe6e0321677f6352be9b12c7f74df28
madsteel63 <silverlightoffallingstar@gmail.com>
2022-02-14 12:48:43 +0400

    Initial commit

---
+++ src/Stage02.java
@@ -0,0 +1,16 @@
+public class Stage02 {
+    public static void main(String[] args) {
+        int i,j;
+        i = j = 3;
+        float k,l;
+        k = l = 3f;
+        System.out.println("Результат арифметического сложения для \"int\" 3 + 3 = " + (i + j));
+        System.out.println("Результат арифметического вычитания для \"int\" 3 - 3 = " + (i - j));
+        System.out.println("Результат арифметического деления для \"int\" 3 / 3 = " + (i / j));
+        System.out.println("Результат арифметического умножения для \"int\" 3 * 3 = " + (i * j));
+        System.out.println("Результат арифметического сложения для \"float\" 3.0 + 3.0 = " + (k + l));
+        System.out.println("Результат арифметического вычитания для \"float\" 3.0 - 3.0 = " + (k - l));
+        System.out.println("Результат арифметического деления для \"float\" 3.0 / 3.0 = " + (k / l));
+        System.out.println("Результат арифметического умножения для \"float\" 3.0 * 3.0 = " + (k * l));
+    }
+}
